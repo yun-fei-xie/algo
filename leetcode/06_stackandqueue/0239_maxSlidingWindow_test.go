aaf7f710dcff6fa940f82baf8f5d8fdcffcd2c10
yun-fei-xie <1527446599@qq.com>
2023-03-14 12:53:17 +0800

    03/14

--- leetcode/06_stackandqueue/0239_maxSlidingWindow_test.go
+++ leetcode/06_stackandqueue/0239_maxSlidingWindow_test.go
@@ -1,15 +1,87 @@
 package _6_stackandqueue
 
+import (
+	"container/list"
+	"fmt"
+	"testing"
+)
+
 /*
 https://leetcode.cn/problems/sliding-window-maximum/
 
+给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+返回 滑动窗口中的最大值 。
+输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+输出：[3,3,5,5,6,7]
+解释：
+滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+
+
+
 如果每次都对窗口中的数字遍历肯定会超时。
 窗口每次移动，会出去一个元素 进来一个元素。-> 非常像一个入队和出队的操作
-我需要用一种方法记住当前队列的最大值（我需要用另个一个队列）
+
+如果每次元素进出，这个队列能告诉我当前队列中所有元素的最大值是什么就可以满足需求。
+
+满足单调性的双端队列被称为单调队列。
+设计单调队列的时候，pop，和push操作要保持如下规则：
+1.pop(value)：如果窗口移除的元素value等于单调队列的出口元素，那么队列弹出元素，否则不用任何操作
+2.push(value)：如果push的元素value大于入口元素的数值，那么就将队列入口的元素弹出，直到push元素的数值小于等于队列入口元素的数值为止
+
+这个题目感觉不是特别难理解，但是需要仔细体会
 
 */
 
-func maxSlidingWindow(nums []int, k int) []int {
+type MonotoneQueue struct {
+	l *list.List
+}
 
-	return nums
+func NewMonotoneQueue() *MonotoneQueue {
+
+	return &MonotoneQueue{l: list.New()}
+}
+
+func (queue *MonotoneQueue) Pop(value int) {
+	if queue.l.Len() != 0 && value == queue.l.Front().Value.(int) {
+		queue.l.Remove(queue.l.Front())
+	}
+}
+
+func (queue *MonotoneQueue) Push(value int) {
+	for queue.l.Len() != 0 && value > queue.l.Back().Value.(int) {
+		queue.l.Remove(queue.l.Back())
+	}
+	queue.l.PushBack(value)
+}
+
+func (queue *MonotoneQueue) GetMax() int {
+	return queue.l.Front().Value.(int)
+}
+
+func maxSlidingWindow(nums []int, k int) []int {
+	queue := NewMonotoneQueue()
+	for i := 0; i < k; i++ {
+		queue.Push(nums[i])
+	}
+	result := make([]int, 0)
+	result = append(result, queue.GetMax())
+	for i := k; i < len(nums); i++ {
+		queue.Pop(nums[i-k]) // 移除滑动窗口最前面的元素（不一定真的会发生物理删除）
+		queue.Push(nums[i])
+		result = append(result, queue.GetMax())
+	}
+
+	return result
+}
+
+func TestMaxSlidingWindow(t *testing.T) {
+	fmt.Println(maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
+	fmt.Println(maxSlidingWindow([]int{1}, 1))
 }
---
+++ leetcode/06_stackandqueue/0347_topKFrequent_test.go
@@ -0,0 +1,69 @@
+package _6_stackandqueue_test
+
+import (
+	"container/heap"
+	"fmt"
+	"testing"
+)
+
+/*
+https://leetcode.cn/problems/top-k-frequent-elements/description/?favorite=2cktkvj
+
+给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+示例 1:
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+
+解法1：基于堆排序  golang的container/heap 实现了heap 把元素和出现的频率封装成一个整体进行比较
+解法2：基于快速排序
+
+*/
+
+type IHeap [][2]int // 0->数子  1->出现的频率
+func (h IHeap) Len() int {
+	return len(h)
+}
+func (h IHeap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+func (h IHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *IHeap) Push(x any) {
+	*h = append(*h, x.([2]int))
+}
+
+// 删除数组中最后一个元素
+func (h *IHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func topKFrequent(nums []int, k int) []int {
+	occurences := map[int]int{} // 初始化map
+	for _, num := range nums {
+		occurences[num]++
+	}
+	h := &IHeap{}
+	heap.Init(h)
+
+	for key, value := range occurences {
+		heap.Push(h, [2]int{key, value})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	ret := make([]int, k)
+	for i := 0; i < k; i++ {
+		ret[k-i-1] = heap.Pop(h).([2]int)[0]
+	}
+	return ret
+}
+
+func TestTopK(t *testing.T) {
+	fmt.Println(topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2))
+}
