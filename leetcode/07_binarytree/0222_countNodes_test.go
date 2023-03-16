c5a9d0bded79116faf9895a296e2e8a043a63185
yun-fei-xie <1527446599@qq.com>
2023-03-16 11:47:04 +0800

    03/16 代码随想录 二叉树打卡

--- leetcode/07_binarytree/0222_countNodes_test.go
+++ leetcode/07_binarytree/0222_countNodes_test.go
@@ -10,6 +10,10 @@ https://leetcode.cn/problems/count-complete-tree-nodes/description/
 
 如何不使用遍历，把时间复杂度压缩到O(1)
 */
+
+/*
+广度优先遍历解法
+*/
 func countNodes(root *TreeNode) int {
 	if root == nil {
 		return 0
@@ -30,4 +34,38 @@ func countNodes(root *TreeNode) int {
 		}
 	}
 	return totalCounts
+}
+
+/*
+递归解法,极简
+但是没有用到完全二叉树的性质
+*/
+
+func countNodes2(root *TreeNode) int {
+
+	var countNodes func(node *TreeNode) int
+	countNodes = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		} else {
+			return countNodes(node.Left) + countNodes(node.Right) + 1
+
+		}
+	}
+	return countNodes(root)
+}
+
+/*
+使用完全二叉树的性质
+
+规定根节点位于第0层，完全二叉树的最大层数为h。
+根据完全二叉树的特性可知，完全二叉树的最左边的节点一定位于最底层。
+因此从根节点出发，每次访问左子节点，直到遇到叶子节点，该叶子节点即为完全二叉树的最左边的节点，经过的路径长度即为最大层数h。
+当h0≤i<h 时，第i层包含2^i个节点，最底层包含的节点数最少为1，最多为2^h
+
+题解参考:https://leetcode.cn/problems/count-complete-tree-nodes/solutions/495655/wan-quan-er-cha-shu-de-jie-dian-ge-shu-by-leetco-2/
+*/
+func countNode3(root *TreeNode) int {
+	//todo
+	return 0
 }
--- leetcode/07_binarytree/0559_maxDepthNTree_test.go
+++ leetcode/07_binarytree/0559_maxDepthNTree_test.go
@@ -3,6 +3,10 @@ package _7_binarytree
 /*
 https://leetcode.cn/problems/maximum-depth-of-n-ary-tree/description/
 
+给定一个 N 叉树，找到其最大深度。
+最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。
+
 */
 
 type Node struct {
