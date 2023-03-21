800db743844fe74a95b496e1fddf074318adef91
yun-fei-xie <1527446599@qq.com>
2023-03-21 22:13:05 +0800

    03/21代码随想录打卡  ● 530.二叉搜索树的最小绝对差 ● 501.二叉搜索树中的众数 ● 236. 二叉树的最近公共祖先

--- leetcode/07_binarytree/0236_lowestCommonAncestor_test.go
+++ leetcode/07_binarytree/0236_lowestCommonAncestor_test.go
@@ -10,9 +10,16 @@ package _7_binarytree
  */
 
 /*
+https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
+
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
 二叉树最近公共祖先问题
 这个题的精髓在于后续遍历过程中返回哪个节点？
 还有一点是，当递归到p节点或者q节点时，直接返回，不需要继续向下递归
+
+递归何时返回-> 1.节点为空、遇到了节点p、遇到了节点q
 */
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 
@@ -26,7 +33,7 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 		right := postOrder(node.Right, p, q)
 
 		// 三种情况
-		if left != nil && right != nil {
+		if left != nil && right != nil { // 1.左右都不为空，自底向上看的话，当前节点就是最小公共祖先
 			return node
 		} else if left != nil {
 			return left
--- leetcode/07_binarytree/0501_findMode_test.go
+++ leetcode/07_binarytree/0501_findMode_test.go
@@ -7,7 +7,52 @@ import (
 
 /*
 https://leetcode.cn/problems/find-mode-in-binary-search-tree/description/
+
+给你一个含重复值的二叉搜索树（BST）的根节点 root ，找出并返回 BST 中的所有 众数（即，出现频率最高的元素）。
+如果树中有不止一个众数，可以按 任意顺序 返回。
+
+假定 BST 满足如下定义：
+结点左子树中所含节点的值 小于等于 当前节点的值
+结点右子树中所含节点的值 大于等于 当前节点的值
+左子树和右子树都是二叉搜索树
+
+1.把数据放到map[value]-> frequency中，然后在map中进行filter。这种做法消耗掉一定的空间。
+2.可以在遍历过程中，记住出现频率最高的val。因为中序遍历是一个有序的过程，相同的元素会排列在一起。
 */
+
+func findMode2(root *TreeNode) []int {
+	res := make([]int, 0)
+	var base, cnt, maxCnt int
+	update := func(val int) {
+
+		if val == base {
+			cnt++ // 这里不要直接return了
+		} else {
+			base, cnt = val, 1
+		}
+		if cnt == maxCnt {
+			res = append(res, val)
+		}
+		if cnt > maxCnt { // 新的众数出现， 应该将前面的值清空
+			res = []int{base}
+		}
+	}
+
+	var preOrder func(node *TreeNode)
+	preOrder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		preOrder(node.Left)
+		update(node.Val)
+		preOrder(node.Right)
+	}
+
+	preOrder(root)
+	return res
+}
+
 func findMode(root *TreeNode) []int {
 
 	var mp = make(map[int]int)
@@ -29,6 +74,7 @@ func findMode(root *TreeNode) []int {
 	}
 	preorder(root)
 
+	// 从map中找出频率最大的值，这样会比较费空间
 	freq := math.MinInt64
 	for _, f := range mp {
 		if f > freq {
